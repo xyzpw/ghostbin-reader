f125627a6887a2452f8ffbd0512dd12745b50275
xyzpw <76017734+xyzpw@users.noreply.github.com>
2023-06-02 00:31:17 -0400

    Create ghostbin-reader.py

---
+++ ghostbin-reader.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import requests, re
+
+replace_list = {"&nbsp;": " ", "&amp;": "&", "&lt;": "<", "&gt;": ">", "&quot;": "\"", "&apos;": "'", "&cent;": "¢", "&pound;": "£", "&yen;": "¥", "&euro;": "€", "&copy;": "©", "&reg;": "®"}
+start_text_match = "<textarea id=\"editor\" name=\"content\">"
+end_text_match = "</textarea>"
+url = input("https://ghostbin.me/")
+
+packet = requests.get(f"https://ghostbin.me/{url}")
+if (start_text_match not in packet.text) or (end_text_match not in packet.text):
+    exit("url is broken for non-existent - exiting")
+
+start = packet.text.find(start_text_match) + len(start_text_match)
+end = packet.text.find(end_text_match)
+
+content = packet.text[start:end]
+for key in replace_list:
+    content = re.sub(key, replace_list[key], content)
+
+print("\n" + content + "\n")
